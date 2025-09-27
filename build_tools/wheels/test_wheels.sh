df9f39ccd73d5ed79745ca4bafbc188c90084ca4
Loïc Estève <loic.esteve@ymail.com>
2025-09-27 06:50:52 +0200

    debug

--- build_tools/wheels/test_wheels.sh
+++ build_tools/wheels/test_wheels.sh
@@ -22,9 +22,9 @@ fi
 # threadpoolctl output section of the show_versions output:
 python -c "import sklearn; sklearn.show_versions()"
 
-if pip show -qq pytest-xdist; then
-    XDIST_WORKERS=$(python -c "import joblib; print(joblib.cpu_count(only_physical_cores=True))")
-    pytest --pyargs sklearn -n $XDIST_WORKERS
-else
-    pytest --pyargs sklearn
-fi
+# if pip show -qq pytest-xdist; then
+#     XDIST_WORKERS=$(python -c "import joblib; print(joblib.cpu_count(only_physical_cores=True))")
+#     pytest --pyargs sklearn -n $XDIST_WORKERS
+# else
+#     pytest --pyargs sklearn
+# fi
